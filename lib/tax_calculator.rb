99151205732d5aed59373f77f5d7a88b8924496f
john connolly <jtconnolly82@gmail.com>
2022-01-20 17:35:00 -0700

    tax rate cont.

--- lib/tax_calculator.rb
+++ lib/tax_calculator.rb
@@ -4,15 +4,17 @@ class TaxCalculator
     @tax_rate = 0
   end
 
-  BASE_RATE = 10
-  IMPORT_RATE = 5
+  BASE_RATE = 0.10
+  IMPORT_RATE = 0.05
 
   EXEMPT_CATEGORIES = ["book", "medicine", "food"]
 
   def get_total_taxes(price_before_tax, item_type, is_imported)
     before_tax_times_tax_rate = price_before_tax * tax_rate(item_type, is_imported)
-    total_tax = before_tax_times_tax_rate / 100
-    rounded_total_tax = (total_tax * 2) / 2.0
+    total_tax = before_tax_times_tax_rate
+    puts "total tax #{total_tax}"
+    rounded_total_tax = (total_tax * 20)/ 20
+    puts "rounded tax #{rounded_total_tax}"
     return rounded_total_tax
   end
 
@@ -23,6 +25,7 @@ class TaxCalculator
     if is_imported == true
       @tax_rate += IMPORT_RATE
     end
+    
     return @tax_rate
   end
 end
